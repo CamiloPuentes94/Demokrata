f449fbd52694442f1f509f7fb78d8f8b8d28ac1b
camilo puentes <jcpsandoval94@gmail.com>
2024-04-06 08:19:14 -0500

    Add IUsuariosServices and UsuariosServices classes

---
+++ Demokrata/Services/IUsuariosServices.cs
@@ -0,0 +1,14 @@
+﻿using Demokrata.Modelo;
+
+namespace Demokrata.Services
+{
+    public interface IUsuariosServices
+    {
+        ICollection<Usuario> GetUsuarios();
+        Usuario GetUsuario(int id);
+        bool CreateUsuario(Usuario usuario);
+        bool UpdateUsuario(Usuario usuario);
+        bool DeleteUsuario(Usuario usuario);
+        ICollection<Usuario> BuscarUsuario(string nombre);
+    }
+}
---
+++ Demokrata/Services/UsuariosServices.cs
@@ -0,0 +1,67 @@
+﻿using Demokrata.Data;
+using Demokrata.Modelo;
+
+namespace Demokrata.Services
+{
+    public class UsuariosServices : IUsuariosServices
+    {
+
+        private readonly aplicacionDbContext _db;
+
+        public UsuariosServices(aplicacionDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateUsuario(Usuario usuario)
+        {
+            usuario.FechaCreacion = DateTime.Now;
+            _db.Usuarios.Add(usuario);
+
+            return Guardar();
+        }
+
+        public bool UpdateUsuario(Usuario usuario)
+        {
+            usuario.FechaModificacion = DateTime.Now;
+            _db.Usuarios.Update(usuario);
+
+            return Guardar();
+        }
+
+        public bool DeleteUsuario(Usuario usuario)
+        {
+            _db.Usuarios.Remove(usuario);
+
+            return Guardar();
+        }
+
+        public ICollection<Usuario> GetUsuarios()
+        {
+            return _db.Usuarios.OrderBy(u => u.Id).ToList();
+        }
+
+        public Usuario GetUsuario(int id)
+        {
+            return _db.Usuarios.FirstOrDefault(u => u.Id == id);
+        }
+
+
+        public ICollection<Usuario> BuscarUsuario(string nombre)
+        {
+            IQueryable<Usuario> query = _db.Usuarios;
+
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                query = query.Where(e => e.PrimerNombre.Contains(nombre) || e.PrimerApellido.Contains(nombre));
+            }
+            return query.ToList();
+        }
+
+
+        public bool Guardar()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+    }
+}
